073f1d3c698beee494912ca0c6883f3288ed4d8f
daria marczak <marczak.ria@gmail.com>
2018-01-19 09:59:03 +0100

    first commit

---
+++ comments.js
@@ -0,0 +1,49 @@
+import {
+  ADD_COMMENT,
+  EDIT_COMMENT,
+  REMOVE_COMMENT,
+  THUMB_UP_COMMENT,
+  THUMB_DOWN_COMMENT
+} from "./actions";
+
+function comments(state = [], action) {
+  switch (action.type) {
+    case ADD_COMMENT:
+      return Object.assign({}, state, {
+        comments: [{
+          id: action.id,
+          text: action.text,
+          votes: 0
+        }, ...state]
+      });
+      
+    case REMOVE_COMMENT:
+      return Object.assign({}, state, {
+        comments: state.comments.filter(comment => comment.id !== action.id)
+      });
+      
+    case EDIT_COMMENT:
+      return Object.assign({}, state, {
+        comments: [state.comments.map((comment => comment.id !== action.id)) {
+          text: action.text
+        }]
+      });
+      
+    case THUMB_UP_COMMENT:
+      return Object.assign({}, state, {
+        comments: [state.comments.map((comment => comment.id !== action.id)) {
+          votes: comment.votes + 1;
+        }]
+      });
+      
+    case THUMB_DOWN_COMMENT:
+      return Object.assign({}, state, {
+        comments: [state.comments.map((comment => comment.id !== action.id)) {
+          votes: comment.votes - 1;
+        }]
+      });
+      
+    default:
+      return state;
+  }
+}
