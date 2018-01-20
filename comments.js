2020a1a374e744f4303d26ae9595514c9f81b4e5
daria marczak <marczak.ria@gmail.com>
2018-01-20 18:04:39 +0100

    Fixed cases

--- comments.js
+++ comments.js
@@ -6,44 +6,32 @@ import {
   THUMB_DOWN_COMMENT
 } from "./actions";
 
-function comments(state = [], action) {
+function reducer(state = [], action) {
   switch (action.type) {
     case ADD_COMMENT:
-      return Object.assign({}, state, {
-        comments: [{
-          id: action.id,
-          text: action.text,
-          votes: 0
-        }, ...state]
-      });
-      
-    case REMOVE_COMMENT:
-      return Object.assign({}, state, {
-        comments: state.comments.filter(comment => comment.id !== action.id)
-      });
-      
+      return [{
+        id: action.id,
+        text: action.text,
+        votes: 0
+      },
+      ...state]
     case EDIT_COMMENT:
-      return Object.assign({}, state, {
-        comments: [state.comments.map((comment => comment.id !== action.id)) {
-          text: action.text
-        }]
-      });
-      
+      return state.map(comment => comment.id === action.id 
+                       ? { id: comment.id, text: action.text } 
+                       : comment)
+    case REMOVE_COMMENT:
+      return state.filter(comment => comment.id !== action.id);
     case THUMB_UP_COMMENT:
-      return Object.assign({}, state, {
-        comments: [state.comments.map((comment => comment.id !== action.id)) {
-          votes: comment.votes + 1;
-        }]
-      });
-      
-    case THUMB_DOWN_COMMENT:
-      return Object.assign({}, state, {
-        comments: [state.comments.map((comment => comment.id !== action.id)) {
-          votes: comment.votes - 1;
-        }]
-      });
-      
+        return state.map(comment => comment.id === action.id
+                        ? {id: comment.id, votes: comment.votes +1}
+                        : comment);
+    case THUMB_UP_COMMENT:
+    return state.map(comment => comment.id === action.id
+                    ? {id: comment.id, votes: comment.votes -1}
+                    : comment);
     default:
-      return state;
+      return state
   }
 }
+
+export default comments;
